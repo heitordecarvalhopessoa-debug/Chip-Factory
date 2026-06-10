6f5941152a0b402615e9f47dff745a269c96c10d
Heitor de Carvalho Pessoa <heitordecarvalhopessoa@gmail.com>
2026-06-10 19:52:28 -0300

    Create helper.js

---
+++ js/helper.js
@@ -0,0 +1,9 @@
+document.addEventListener('keydown', (e) => {
+    if (e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') return;
+
+    if (e.key === 'Control') {
+        selectTool('pan');
+    } else if (e.key === 'Shift') {
+        selectTool('link');
+    }
+});
